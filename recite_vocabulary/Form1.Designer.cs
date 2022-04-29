45d8b58ac815229b8aa69a23cbc2d82f87d33e54
judgeyjj <2179067329@qq.com>
2022-04-29 08:06:45 +0800

    Signed-off-by: judgeyjj <2179067329@qq.com>

---
+++ recite_vocabulary/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace recite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Second = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Minute = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Hour = new System.Windows.Forms.Label();
+            this.Chinese = new System.Windows.Forms.Label();
+            this.indexs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.vocabulary = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vocabulary_num = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vocabulary_num)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Second);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.Minute);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.Hour);
+            this.panel1.Controls.Add(this.Chinese);
+            this.panel1.Controls.Add(this.indexs);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.start);
+            this.panel1.Controls.Add(this.next);
+            this.panel1.Controls.Add(this.vocabulary);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.vocabulary_num);
+            this.panel1.Location = new System.Drawing.Point(18, 20);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(676, 460);
+            this.panel1.TabIndex = 0;
+            // 
+            // Second
+            // 
+            this.Second.AutoSize = true;
+            this.Second.Location = new System.Drawing.Point(571, 172);
+            this.Second.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Second.Name = "Second";
+            this.Second.Size = new System.Drawing.Size(27, 20);
+            this.Second.TabIndex = 15;
+            this.Second.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(546, 172);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = ":";
+            // 
+            // Minute
+            // 
+            this.Minute.AutoSize = true;
+            this.Minute.Location = new System.Drawing.Point(507, 172);
+            this.Minute.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Minute.Name = "Minute";
+            this.Minute.Size = new System.Drawing.Size(27, 20);
+            this.Minute.TabIndex = 13;
+            this.Minute.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(486, 172);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = ":";
+            // 
+            // Hour
+            // 
+            this.Hour.AutoSize = true;
+            this.Hour.Location = new System.Drawing.Point(451, 172);
+            this.Hour.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Hour.Name = "Hour";
+            this.Hour.Size = new System.Drawing.Size(27, 20);
+            this.Hour.TabIndex = 11;
+            this.Hour.Text = "00";
+            // 
+            // Chinese
+            // 
+            this.Chinese.AutoSize = true;
+            this.Chinese.Location = new System.Drawing.Point(69, 293);
+            this.Chinese.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Chinese.Name = "Chinese";
+            this.Chinese.Size = new System.Drawing.Size(0, 20);
+            this.Chinese.TabIndex = 10;
+            // 
+            // indexs
+            // 
+            this.indexs.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.indexs.Location = new System.Drawing.Point(141, 130);
+            this.indexs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.indexs.Name = "indexs";
+            this.indexs.Size = new System.Drawing.Size(32, 25);
+            this.indexs.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(181, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "个";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(106, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "第";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(386, 75);
+            this.start.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(112, 38);
+            this.start.TabIndex = 6;
+            this.start.Text = "开始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(91, 172);
+            this.next.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(106, 38);
+            this.next.TabIndex = 5;
+            this.next.Text = "不记得";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // vocabulary
+            // 
+            this.vocabulary.Location = new System.Drawing.Point(231, 172);
+            this.vocabulary.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.vocabulary.Name = "vocabulary";
+            this.vocabulary.Size = new System.Drawing.Size(199, 27);
+            this.vocabulary.TabIndex = 3;
+            this.vocabulary.TextChanged += new System.EventHandler(this.vocabulary_TextChanged);
+            this.vocabulary.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.vocabulary_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(218, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(309, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "请输入以下中文对应的英文单词：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(58, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入这次背的单词数量：";
+            // 
+            // vocabulary_num
+            // 
+            this.vocabulary_num.Location = new System.Drawing.Point(305, 82);
+            this.vocabulary_num.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.vocabulary_num.Name = "vocabulary_num";
+            this.vocabulary_num.Size = new System.Drawing.Size(63, 27);
+            this.vocabulary_num.TabIndex = 0;
+            this.vocabulary_num.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 500);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vocabulary_num)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox indexs;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.TextBox vocabulary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown vocabulary_num;
+        private System.Windows.Forms.Label Chinese;
+        private System.Windows.Forms.Label Second;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label Minute;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Hour;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ recite_vocabulary/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Random;
+
+namespace recite_vocabulary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int num;
+        public int index=0;
+        public string word;
+        public string meaning;
+        public int correct = 0;
+
+        private void vocabularies(string voca_path)
+        {
+            StreamReader sr = new StreamReader(voca_path, Encoding.Default);
+            string str;
+            while((str = sr.ReadLine()) != null)
+            {
+                
+            }
+            sr.Close();
+        }
+        private void next_vocabulary(object sender, EventArgs e)
+        {
+            vocabulary.Text = "";
+            indexs.Text = (index+1).ToString();
+            
+            Random rd = new Random();
+            int every = rd.Next(0, i);
+
+            string[] temp = new string[3];
+            temp = strl[every].Split(new Char[] { '\t', '\t', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            word = temp[0];
+            meaning = temp[1];
+
+            Chinese.Text = meaning;
+
+            index++;
+
+            next.Text = "记不住";
+            
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            next.Enabled = true;
+            timer1.Start();
+            Chinese.Text = "";
+            num = (int)vocabulary_num.Value;
+
+            this.next_vocabulary(sender, e);
+            
+        }
+        string[] strl = new string[1409];
+        public int i = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            next.Enabled = false;
+            StreamReader sr = new StreamReader("College_Grade4.txt", Encoding.Default);
+            string str;
+            while ((str = sr.ReadLine()) != null)
+            {      
+                strl[i] = str;
+                i++;
+            }
+
+            Chinese.Text = "目前本系统共有" + i.ToString() + "个单词";
+
+            //Chinese.Text = strl[0];
+            string[] temp = new string[3];
+            temp = strl[0].Split(new Char[] { '\t', '\t', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            if(next.Text == "记不住")
+            {
+                vocabulary.Text = word;
+                vocabulary.Enabled = false;
+                if (index == num)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("任务完成！共记住"+correct+"个单词");
+                    next.Enabled = false;
+                }
+                next.Text = "下一个";
+            }
+            else
+            {
+                vocabulary.Enabled = true;
+                this.next_vocabulary(sender, e);
+            }
+        }
+
+
+        
+        private void vocabulary_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void vocabulary_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if(vocabulary.Text == word)
+                {
+                    correct++;
+                    this.next_vocabulary(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("拼写错误");
+                }
+            }
+        }
+
+        int second = 0;
+        int minute = 0;
+        int hour = 0;
+
+        string string_s;
+        string string_m;
+        string string_h;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second++;
+            if (second == 60)
+            {
+                second = 0;
+                minute++;
+            }
+            if(minute == 60)
+            {
+                minute = 0;
+                hour++;
+            }
+
+            if (second < 10)
+            {
+                string_s = "0" + second.ToString();
+            }
+            else
+            {
+                string_s = second.ToString();
+            }
+            if (minute < 10)
+            {
+                string_m = "0" + minute.ToString();
+            }
+            else
+            {
+                string_m = minute.ToString();
+            }
+            if (hour < 10)
+            {
+                string_h = "0" + hour.ToString();
+            }
+            else
+            {
+                string_h = hour.ToString();
+            }
+            Hour.Text = string_h;
+            Minute.Text = string_m;
+            Second.Text = string_s;
+        }
+    }
+}
